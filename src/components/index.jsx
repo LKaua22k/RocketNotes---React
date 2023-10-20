fae25f7136389f8da3a45294a38c7323b1f77476
Kauã Araujo de Lima <lkauan462@gmail.com>
2023-10-20 14:38:22 +0000

    Fazendo o primeiro componnent :tada:

--- src/components/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "./style";
-
-export default function Button(){
-    return(
-        <Container type="button">
-            Meu botão
-        </Container>
-    )
-}
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,14 @@ import GlobalStyles from './Styles/global'
 import theme from './Styles/theme'
 
 import {Details} from './Pages/Details'
-import Button from './components'
+import Button from './Components'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Details />
+      <Button title='Acesse ja' loading/>
       <Button />
     </ThemeProvider>
   </React.StrictMode>,
