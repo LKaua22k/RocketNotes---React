f27acdbd05146c41911c419c7426e4161e2af338
Kauã Araujo de Lima <lkauan462@gmail.com>
2023-10-23 19:45:00 +0000

    Criando a seção de Profile :tada:

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,9 +1,15 @@
-import {Container} from './style'
+import {Container , Profile} from './style'
 
 export function Header(){
     return(
            <Container>
-            
+            <Profile>
+                <img src="https://github.com/LKaua22k.png" alt="Imagem do usuario" />
+                <div>
+                    <span>Texto 1</span>
+                    <strong>Texto 2</strong>
+                </div>
+            </Profile>
            </Container> 
         )
 }
