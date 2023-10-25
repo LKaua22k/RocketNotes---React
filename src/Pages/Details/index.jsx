94ed33036633afd9f350c368d5700a17511d6e4a
Kauã Araujo de Lima <lkauan462@gmail.com>
2023-10-25 13:39:30 +0000

    Criando o componente de Section :tada:

--- src/Pages/Details/index.jsx
+++ src/Pages/Details/index.jsx
@@ -2,6 +2,7 @@ import {Container} from './style'
 
 import {Header} from '../../Components/Header'
 import {Button} from '../../Components/Buttons'
+import {Section} from '../../Components/Section'
 
 
 export function Details() {
@@ -9,6 +10,17 @@ export function Details() {
   return(
     <Container>
       <Header />
+
+      <Section title='Links úteis'>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+          </ul>
+      </Section>
+
+      <Section title='Marcadores'>
+        
+      </Section>
 
       <Button title='Voltar'></Button>
     </Container>
