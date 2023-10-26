6c2bf418c45d3cba1b485afefd46166c3aa7adda
Kauã Araujo de Lima <lkauan462@gmail.com>
2023-10-26 19:47:29 +0000

    Terminando a interface de details :tada:

--- src/Pages/Details/style.js
+++ src/Pages/Details/style.js
@@ -9,7 +9,23 @@ export const Container = styled.div`
     grid-template-rows: 105px auto;
     grid-template-areas: 
     "header"
-    "content"
-    ;
+    "content";
+
+    > main{
+        grid-area: content;
+        padding: 64px 0;
+        overflow-y: scroll;
+    }
     
+`;
+
+export const Content = styled.div`
+    max-width: 550px;
+    display: flex;
+    flex-direction: column;
+    margin:0 auto;
+
+    > button:first-child{
+        align-self: end;
+    }
 `;
