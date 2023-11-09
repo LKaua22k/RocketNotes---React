49e6e620274b5bf949c482f28bea86c424f69d18
Lkaua22k <lkauan462@gmail.com>
2023-11-09 16:44:26 -0300

    Criando um NoteItem e a pagina de New :tada:

--- src/Pages/New/index.jsx
+++ src/Pages/New/index.jsx
@@ -3,6 +3,8 @@ import { Container , Form } from "./style";
 import {Header} from '../../Components/Header'
 import {Input} from '../../Components/Input'
 import {Textarea} from '../../Components/Textarea'
+import {Section} from '../../Components/Section'
+import {NoteItem} from '../../Components/NoteItem'
 
 export function New(){
     return(
@@ -17,6 +19,13 @@ export function New(){
 
                     <Input placeholder="Titulo" type="text" />
                     <Textarea placeholder="Observações" />
+
+                    <Section title='Links úteis'/>
+                    <NoteItem value='https://www.rocketseat.com.br'/>
+                    <NoteItem  isNew placeholder='Novo link'/>
+
+                    <Section title='Marcadores'/>
+
                 </Form>
             </main>
         </Container>
